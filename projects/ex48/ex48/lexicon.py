73e3d13515a09fa7ba6c4679236115bd0bce8439
Dima <trolzface@gmail.com>
2016-09-27 23:46:04 +0000

    lol

--- projects/ex48/ex48/lexicon.py
+++ projects/ex48/ex48/lexicon.py
@@ -1,8 +1,10 @@
 def scan(s):
       directions = ('north', 'south', 'east', 'west')
+     # s = s.lower()
+     # s = s.split()
       for dir in directions:
         if dir == s:
-            return('direction', dir)
+            return [('direction',dir)]
         return ('error', None)
 
 
