37716e2eafaf81c00bc7887c3437ea1e68f908a0
Dima <trolzface@gmail.com>
2016-09-28 03:29:01 +0000

    first test is a go

--- projects/ex48/ex48/lexicon.py
+++ projects/ex48/ex48/lexicon.py
@@ -1,13 +1,20 @@
 def scan(s):
-      directions = ('north', 'south', 'east', 'west')
-     # s = s.lower()
-     # s = s.split()
-      for dir in directions:
-        if dir == s:
-            return [('direction',dir)]
-        return ('error', None)
+    directions = ('north', 'south', 'east', 'west')
+    s = s.lower()
+    s = s.split()
+    sout = []
+    for sin in s:
+        found = 0
+        for dir in directions:
+            if dir == sin:
+                sout.append(('direction', dir))
+                found = 1
+                break
+        if found == 0:
+            # return [('direction',dir)]
+            sout.append(('error', None))
+    return sout
 
-
-#print scan('north')
+#print scan('north east south')
 
 
