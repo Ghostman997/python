290a577fea33271a919cc0045bfa7e1ab814b859
Dima <trolzface@gmail.com>
2016-09-28 22:37:28 +0000

    ex48

--- projects/ex48/ex48/lexicon.py
+++ projects/ex48/ex48/lexicon.py
@@ -1,6 +1,9 @@
 def scan(s):
     directions = ('north', 'south', 'east', 'west')
-    s = s.lower()
+    verbs = ('go', 'eat', 'kill')
+    stops = ('the', 'in', 'of')
+    nouns = ('bear', 'princess')
+   #s = s.lower()
     s = s.split()
     sout = []
     for sin in s:
@@ -10,11 +13,36 @@ def scan(s):
                 sout.append(('direction', dir))
                 found = 1
                 break
+        for ver in verbs:
+            if ver == sin:
+                sout.append(('verb', ver))
+                found = 1
+                break
+        for sto in stops:
+            if sto == sin:
+                sout.append(('stop', sto))
+                found = 1
+                break
+        for nou in nouns:
+            if nou == sin:
+                sout.append(('noun', nou))
+                found = 1
+                break
+        try:
+            numbers = int(sin)
+            sout.append(('number', numbers))
+            found = 1
+            break
+        except ValueError:
+            pass
         if found == 0:
             # return [('direction',dir)]
-            sout.append(('error', None))
+            sout.append(('error', sin))
     return sout
 
 #print scan('north east south')
-
-
+#print scan('go eat kill')
+#print scan('the in of')
+#print scan('bear princess')
+print scan('bear IAS princess')
+print scan("3 91234")
